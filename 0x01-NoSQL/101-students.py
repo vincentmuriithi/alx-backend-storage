598c072364c33f033ea648908d0048bdd7d2f242
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-05-14 12:40:10 -0700

    102-log_stats.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 """
-students
+Return all students sorted by average score
+Prototype: def top_students(mongo_collection):
+The top must be ordered
+average score retuens with key = averageScore
 """
 
 
 def top_students(mongo_collection):
-    """ students by score """
+    """
+    Prototype: def top_students(mongo_collection):
+    Returns all students sorted by average score
+    """
     return mongo_collection.aggregate([
         {
             "$project":
-                {
-                    "name": "$name",
-                    "averageScore": {"$avg": "$topics.score"}
-                }
+            {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
         },
         {
             "$sort":
-                {
-                    "averageScore": -1
-                }
+            {
+                "averageScore": -1
+            }
         }
     ])
--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3
-""" Log stats - new version """
+"""Improved log stats module"""
 from pymongo import MongoClient
 
 
-def nginx_stats_check():
-    """ provides some stats about Nginx logs stored in MongoDB:"""
-    client = MongoClient()
-    collection = client.logs.nginx
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs.nginx
 
-    num_of_docs = collection.count_documents({})
-    print("{} logs".format(num_of_docs))
+    num_logs = db.count_documents({})
+    print(f"{num_logs} logs")
+
+    get = db.count_documents({'method': 'GET'})
+    post = db.count_documents({'method': 'POST'})
+    put = db.count_documents({'method': 'PUT'})
+    patch = db.count_documents({'method': 'PATCH'})
+    delete = db.count_documents({'method': 'DELETE'})
+
     print("Methods:")
-    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods_list:
-        method_count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status = collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status))
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+
+    status = db.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{status} status check")
 
     print("IPs:")
-
-    top_IPs = collection.aggregate([
+    ips = db.aggregate([
         {"$group":
-         {
-             "_id": "$ip",
-             "count": {"$sum": 1}
-         }
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
          },
         {"$sort": {"count": -1}},
         {"$limit": 10},
@@ -35,11 +42,6 @@ def nginx_stats_check():
             "count": 1
         }}
     ])
-    for top_ip in top_IPs:
-        count = top_ip.get("count")
-        ip_address = top_ip.get("ip")
-        print("\t{}: {}".format(ip_address, count))
 
-
-if __name__ == "__main__":
-    nginx_stats_check()
+    for ip in ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
